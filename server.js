bd613e7b2ec8b2ad90af6c1dc3be082090894d34
dom <crhicko@protonmail.com>
2021-12-08 15:05:20 -0500

    Regsiter Users Added

--- server.js
+++ server.js
@@ -2,12 +2,31 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
+const passport = require('passport')
+const passportLocalStrategy = require('passport-local').Strategy
+const cookieParser = require('cookie-parser')
+const bcrypt = require('bcrypt')
+const session = require('express-session')
+const connectionPool = require('./util/dbConnector')
 
 const app = express()
 const port = 4000
 
-app.use(cors())
+app.use(cors(
+  {credentials: true}
+))
 app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use(session({     //documentation
+  secret: "secretcode",
+  resave: true,
+  saveUninitialized: true
+}));
+
+app.use(cookieParser("secretcode"))
+
+
 
 app.listen(port, () => {
     console.log(`Success! Your application is running on port ${port}.`);
@@ -16,5 +35,27 @@ app.listen(port, () => {
 app.get('/', (req, res) => {
     res.send("wilkommen");
 })
+
+app.post('/login', (req,res) => {
+});
+
+//TODO: Sanitize
+app.post('/register', (req,res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    console.log(hash)
+    connectionPool.query('INSERT INTO users (username, passhash, email) VALUES($1,$2,$3)', [req.body.username, hash, req.body.email], (err, results) => {
+      if (err)
+        throw err
+      res.status(200).json(req.body)
+    })
+  })
+
+});
+
+app.get('/user', (req,res) => {
+  console.log(req.body)
+});
+
+
 
 app.use('/flies', require('./routes/flies'))
--- sql/queries.sql
+++ sql/queries.sql
@@ -2,10 +2,11 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE users(
 	id uuid UNIQUE DEFAULT uuid_generate_v4() NOT NULL,
-	username VARCHAR(30) NOT NULL,
+	username VARCHAR(30) UNIQUE NOT NULL,
 	first_name VARCHAR(30),
 	last_name VARCHAR(30),
-	email VARCHAR(30) NOT NULL
+	email VARCHAR(30) UNIQUE NOT NULL,
+	passhash VARCHAR(128) NOT NULL
 );
 
 CREATE TABLE flies(
