5bee7d87c4c1ac6152dd0493f048bbcf5d9320be
dom <crhicko@protonmail.com>
2022-01-24 14:50:59 -0500

    add a test aws endpoint, setup aws connection

--- routes/flies.js
+++ routes/flies.js
@@ -4,19 +4,24 @@ const router = express.Router()
 
 module.exports = router
 
+const fly_and_tag_query = "Select flies.*, a.tag_list from flies LEFT JOIN (SELECT fly_tags.fly_id as fly_id, string_agg(tags.title, ',' order by tags.title) as tag_list from fly_tags LEFT JOIN tags ON fly_tags.tag_id=tags.id GROUP BY fly_tags.fly_id) a ON flies.id=a.fly_id"
+const fly_and_tag_and_favorites_query = "Select flies.*, CASE WHEN EXISTS (SELECT fly_id FROM favorites WHERE user_id=$1 and fly_id=flies.id) THEN TRUE ELSE FALSE END AS is_favorite, a.tag_list from flies LEFT JOIN (SELECT fly_tags.fly_id as fly_id, string_agg(tags.title, ',' order by tags.title) as tag_list from fly_tags LEFT JOIN tags ON fly_tags.tag_id=tags.id GROUP BY fly_tags.fly_id) a ON flies.id=a.fly_id"
+
 router.get('/', (req, res) => {
     if(req.user) {
         console.log("Looking for user flies xd")
-        connectionPool.query('SELECT f.*, CASE WHEN EXISTS (SELECT fly_id FROM favorites WHERE user_id=$1 and fly_id=f.id) THEN TRUE ELSE FALSE END AS is_favorite FROM flies f ORDER BY id ASC LIMIT 5', [req.user.id], (err, results) => {
+        connectionPool.query(fly_and_tag_and_favorites_query + ' LIMIT 5', [req.user.id], (err, results) => {
         // connectionPool.query('SELECT FLIES.*, favorites.fly_id from FLIES LEFT JOIN favorites ON FLIES.id=favorites.fly_id AND favorites.user_id=$1 ORDER BY id ASC LIMIT 5', [req.user.id], (err, results) => {
             if (err)
                 throw err
             res.status(200).json(results.rows)
         })
     }
+
+
     else {
         console.log("Looking for all flies")
-        connectionPool.query('SELECT * FROM flies LIMIT 10', (err, results) => {
+        connectionPool.query(fly_and_tag_query + ' LIMIT 10', (err, results) => {
             if (err)
                 throw err
             res.status(200).json(results.rows)
--- server.js
+++ server.js
@@ -9,9 +9,22 @@ const bcrypt = require('bcrypt')
 const session = require('express-session')
 const connectionPool = require('./util/dbConnector')
 const setUserAuthCookie = require('./util/setUserAuthCookie')
+const S3 = require('aws-sdk/clients/s3')
 
 const app = express()
 const port = 4000
+
+//S3 Setup
+const s3 = new S3({
+  apiVersion: '2006-03-01',
+  region: 'us-east-1',
+  credentials: {
+    accessKeyId: process.env.AWS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET
+  }
+});
+
+console.log("Region: ", s3.config.region);
 
 console.log(process.env.UI_URL)
 app.use(cors(
@@ -43,6 +56,17 @@ app.listen(port, () => {
 
 app.get('/', (req, res) => {
     res.send("wilkommen");
+})
+
+app.get('/test', (req, res) => {
+    s3.getObject({Bucket: process.env.AWS_BUCKET, Key: 'img/admin/3x.png'}, (err, data) => {
+      if (err) console.log(err, err.stack); // an error occurred
+      else     console.log(data);           // successful response
+    })
+    s3.listObjects({Bucket: 'fly-box'} , (err, data) => {
+      if (err) console.log(err, err.stack);
+      else console.log(data);
+    });
 })
 
 app.post('/login', (req, res, next) => {
