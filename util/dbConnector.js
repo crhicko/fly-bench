95e6031ced35dd7dec7a60707abee8a91b75dfbc
dom <crhicko@protonmail.com>
2022-03-07 22:31:31 -0500

    Trying to fix issue on stale connection caused by cloud provider that terminates tcp connections after downtime

--- util/dbConnector.js
+++ util/dbConnector.js
@@ -1,6 +1,8 @@
 const knex = require('knex')({
     client: 'pg',
-    connection: process.env.DATABASE_URL
+    connection: process.env.DATABASE_URL,
+    keepAlive: true,
+    keepAliveInitialDelayMillis: 10000,
   })
 
 module.exports = knex
